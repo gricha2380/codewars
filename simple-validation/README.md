3f3937310653579c46854c50a671334b27d5a718
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-01 08:38:18 -0800

    simple validation code challenge

---
+++ simple-validation/README.md
@@ -0,0 +1,2 @@
+Regex + Javascript
+https://www.codewars.com/kata/simple-validation-of-a-username-with-regex/train/javascript
---
+++ simple-validation/solution.js
@@ -0,0 +1,7 @@
+let validateUsr = (username) => /^[a-z\d\_]{4,16}$/.test(username);
+
+/*
+function validateUsr(username) {
+  return /^[a-z\d\_]{4,16}$/.test(username);
+}
+*/
