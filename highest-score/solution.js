19436a4eb74c3c8dad67b06b38ab91a7ad8fff90
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-14 06:01:46 -0800

    adds working solution for highest score

--- highest-score/solution.js
+++ highest-score/solution.js
@@ -1,13 +1,20 @@
 function high(x){
-    x.split(' ') // split on each word
-      .sort(function (a,b) {
-        getChars(a) < getChars(b)
-      })
-  }
-  // map and reduce?
-  
-  
-  function getChars(word) {
-    // within each word map the characters according to the char value
-   return [word].map(e=>e.charCodeAt()-96).reduce((acc, cur)=>acc+cur, 0)
+    if (x.length<1) return ''; // return blank string if empty
+    let arr = x.split(' '); //make array of words
+    let hiScore = 0, topWord = '';
+     
+    for (let i = 0; i < arr.length; i++) {
+        // check each char, return char code #, find word sum
+        let score = arr[i]
+          .split('')
+              .map((e) => { return e.charCodeAt(0) - 96})
+                      .reduce((acc, cur) => acc + cur, 0);
+       
+      if (score > hiScore) {
+        // check word sum against hiScore value
+        hiScore = score;
+        topWord = arr[i];
+      }
+    }
+    return topWord; // return largest after loop
   }
